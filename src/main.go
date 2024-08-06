1e031519ecaa64fe40757b4de9d8482541f44951
Andrii Maslovskyi <amaslovskyi@eatery.club>
2024-08-06 16:41:27 +0300

    add: go app, docker, deployment, configmap

---
+++ src/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", helloHandler)
+    
+    fmt.Println("Server is running on http://localhost:8080")
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        fmt.Printf("Error starting server: %s\n", err)
+    }
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, World!")
+}
