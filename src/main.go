932669a59c93403dc578b203d57294267e71aa01
Andrii Maslovskyi <amaslovskyi@eatery.club>
2024-08-06 20:05:57 +0300

    add: secret & secret handler in go app

--- src/main.go
+++ src/main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
-    http.HandleFunc("/", helloHandler)
-    
-    fmt.Println("Server is running on http://localhost:8080")
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        fmt.Printf("Error starting server: %s\n", err)
-    }
+	apiKey := os.Getenv("API_KEY")
+	if apiKey == "" {
+		fmt.Println("API_KEY not set")
+		return
+	}
+
+	http.HandleFunc("/", helloHandler)
+
+	fmt.Println("Server is running on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, World!")
+	fmt.Fprintf(w, "Hello, World! API Key: %s", os.Getenv("API_KEY"))
 }
